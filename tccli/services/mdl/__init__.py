b672fda4c41dd417134266cbb6d675f799ae5551
tencentcloudapi <tencentcloudapi@tenent.com>
2020-10-16 10:48:43 +0800

    release 3.0.101.1

--- tccli/services/mdl/__init__.py
+++ tccli/services/mdl/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from tccli.services.mdl.mdl_client import register_arg
-from tccli.services.mdl.mdl_client import get_actions_info
-from tccli.services.mdl.mdl_client import AVAILABLE_VERSION_LIST
+
+from tccli.services.mdl.mdl_client import action_caller
+    
