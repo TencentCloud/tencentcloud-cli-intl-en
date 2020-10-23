906343cf5416ac02393d8af73d45afb3a997b9d6
tencentcloudapi <tencentcloudapi@tenent.com>
2020-10-23 09:36:09 +0800

    release 3.0.106.1

--- tccli/services/vpc/vpc_client.py
+++ tccli/services/vpc/vpc_client.py
@@ -2188,6 +2188,31 @@ def doDeleteAssistantCidr(args, parsed_globals):
     FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doDescribeBandwidthPackageBillUsage(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey])
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.VpcClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DescribeBandwidthPackageBillUsageRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.DescribeBandwidthPackageBillUsage(model)
+    result = rsp.to_json_string()
+    try:
+        jsonobj = json.loads(result)
+    except TypeError as e:
+        jsonobj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", jsonobj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doDescribeBandwidthPackageResources(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -4886,6 +4911,7 @@ ACTION_MAP = {
     "ModifyVpnGatewayCcnRoutes": doModifyVpnGatewayCcnRoutes,
     "DetachNetworkInterface": doDetachNetworkInterface,
     "DeleteAssistantCidr": doDeleteAssistantCidr,
+    "DescribeBandwidthPackageBillUsage": doDescribeBandwidthPackageBillUsage,
     "DescribeBandwidthPackageResources": doDescribeBandwidthPackageResources,
     "DeleteNetworkInterface": doDeleteNetworkInterface,
     "DescribeVpnConnections": doDescribeVpnConnections,
