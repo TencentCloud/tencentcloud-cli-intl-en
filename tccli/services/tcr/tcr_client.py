508b2381bbf62e76376828b1478b2d0394fd5ab6
tencentcloudapi <tencentcloudapi@tencent.com>
2021-10-09 12:28:14 +0800

    release 3.0.323.1

--- tccli/services/tcr/tcr_client.py
+++ tccli/services/tcr/tcr_client.py
@@ -175,6 +175,33 @@ def doCreateMultipleSecurityPolicy(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doModifyInstance(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(
+        g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+    )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TcrClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.ModifyInstanceRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.ModifyInstance(model)
+    result = rsp.to_json_string()
+    try:
+        json_obj = json.loads(result)
+    except TypeError as e:
+        json_obj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doDeleteMultipleSecurityPolicy(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -219,6 +246,7 @@ ACTION_MAP = {
     "ModifyImmutableTagRules": doModifyImmutableTagRules,
     "DeleteImmutableTagRules": doDeleteImmutableTagRules,
     "CreateMultipleSecurityPolicy": doCreateMultipleSecurityPolicy,
+    "ModifyInstance": doModifyInstance,
     "DeleteMultipleSecurityPolicy": doDeleteMultipleSecurityPolicy,
 
 }
