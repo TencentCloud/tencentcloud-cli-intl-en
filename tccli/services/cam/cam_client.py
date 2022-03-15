25b989fb7cf02a12164e45041f8e93242ce15844
tencentcloudapi <tencentcloudapi@tencent.com>
2022-03-15 09:53:22 +0800

    release 3.0.424.1

--- tccli/services/cam/cam_client.py
+++ tccli/services/cam/cam_client.py
@@ -15,6 +15,33 @@ from tencentcloud.cam.v20190116 import cam_client as cam_client_v20190116
 from tencentcloud.cam.v20190116 import models as models_v20190116
 
 
+def doGetUserAppId(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(
+        g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+    )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.CamClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.GetUserAppIdRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.GetUserAppId(model)
+    result = rsp.to_json_string()
+    try:
+        json_obj = json.loads(result)
+    except TypeError as e:
+        json_obj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doSetMfaFlag(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -1862,6 +1889,7 @@ MODELS_MAP = {
 }
 
 ACTION_MAP = {
+    "GetUserAppId": doGetUserAppId,
     "SetMfaFlag": doSetMfaFlag,
     "ListUsersForGroup": doListUsersForGroup,
     "PutRolePermissionsBoundary": doPutRolePermissionsBoundary,
