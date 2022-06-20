4d928567bbb76d46ad71ef04b891fbee48059e71
tencentcloudapi <tencentcloudapi@tencent.com>
2022-06-20 17:07:42 +0800

    release 3.0.484.1

--- tccli/services/cvm/cvm_client.py
+++ tccli/services/cvm/cvm_client.py
@@ -1203,6 +1203,33 @@ def doModifyInstancesProject(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doExportImages(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    cred = credential.Credential(
+        g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+    )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.CvmClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.ExportImagesRequest()
+    model.from_json_string(json.dumps(args))
+    rsp = client.ExportImages(model)
+    result = rsp.to_json_string()
+    try:
+        json_obj = json.loads(result)
+    except TypeError as e:
+        json_obj = json.loads(result.decode('utf-8'))  # python3.3
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doResetInstance(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -1744,6 +1771,7 @@ ACTION_MAP = {
     "DescribeImportImageOs": doDescribeImportImageOs,
     "InquirePricePurchaseReservedInstancesOffering": doInquirePricePurchaseReservedInstancesOffering,
     "ModifyInstancesProject": doModifyInstancesProject,
+    "ExportImages": doExportImages,
     "ResetInstance": doResetInstance,
     "SyncImages": doSyncImages,
     "DeleteDisasterRecoverGroups": doDeleteDisasterRecoverGroups,
