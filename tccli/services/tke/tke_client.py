5961d7cfa11d4208a3723001f4f787ee528279b8
tencentcloudapi <tencentcloudapi@tencent.com>
2022-09-19 18:06:17 +0800

    release 3.0.543.1

--- tccli/services/tke/tke_client.py
+++ tccli/services/tke/tke_client.py
@@ -867,7 +867,7 @@ def doDescribeEdgeClusterExtraArgs(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doAddNodeToNodePool(args, parsed_globals):
+def doDescribeEdgeLogSwitches(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -894,11 +894,11 @@ def doAddNodeToNodePool(args, parsed_globals):
     client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.AddNodeToNodePoolRequest()
+    model = models.DescribeEdgeLogSwitchesRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.AddNodeToNodePool(model)
+        rsp = client.DescribeEdgeLogSwitches(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -1867,7 +1867,7 @@ def doDeleteEdgeCVMInstances(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doDescribeEdgeLogSwitches(args, parsed_globals):
+def doAddNodeToNodePool(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -1894,11 +1894,11 @@ def doDescribeEdgeLogSwitches(args, parsed_globals):
     client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.DescribeEdgeLogSwitchesRequest()
+    model = models.AddNodeToNodePoolRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.DescribeEdgeLogSwitches(model)
+        rsp = client.AddNodeToNodePool(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -2099,6 +2099,56 @@ def doAddExistedInstances(args, parsed_globals):
     start_time = time.time()
     while True:
         rsp = client.AddExistedInstances(model)
+        result = rsp.to_json_string()
+        try:
+            json_obj = json.loads(result)
+        except TypeError as e:
+            json_obj = json.loads(result.decode('utf-8'))  # python3.3
+        if not g_param[OptionsDefine.Waiter] or search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj) == g_param['OptionsDefine.WaiterInfo']['to']:
+            break
+        cur_time = time.time()
+        if cur_time - start_time >= g_param['OptionsDefine.WaiterInfo']['timeout']:
+            raise ClientError('Request timeout, wait `%s` to `%s` timeout, last request is %s' %
+            (g_param['OptionsDefine.WaiterInfo']['expr'], g_param['OptionsDefine.WaiterInfo']['to'],
+            search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj)))
+        else:
+            print('Inquiry result is %s.' % search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj))
+        time.sleep(g_param['OptionsDefine.WaiterInfo']['interval'])
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doModifyClusterEndpointSP(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
+        cred = credential.CVMRoleCredential()
+    elif g_param[OptionsDefine.RoleArn.replace('-', '_')] and g_param[OptionsDefine.RoleSessionName.replace('-', '_')]:
+        cred = credential.STSAssumeRoleCredential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.RoleArn.replace('-', '_')],
+            g_param[OptionsDefine.RoleSessionName.replace('-', '_')]
+        )
+    elif os.getenv(OptionsDefine.ENV_TKE_REGION)             and os.getenv(OptionsDefine.ENV_TKE_PROVIDER_ID)             and os.getenv(OptionsDefine.ENV_TKE_IDENTITY_TOKEN_FILE)             and os.getenv(OptionsDefine.ENV_TKE_ROLE_ARN):
+        cred = credential.DefaultTkeOIDCRoleArnProvider().get_credentials()
+    else:
+        cred = credential.Credential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+        )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint],
+        proxy=g_param[OptionsDefine.HttpsProxy.replace('-', '_')]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="HmacSHA256")
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.ModifyClusterEndpointSPRequest()
+    model.from_json_string(json.dumps(args))
+    start_time = time.time()
+    while True:
+        rsp = client.ModifyClusterEndpointSP(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -3417,7 +3467,7 @@ def doDescribeClusterNodePools(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doModifyClusterEndpointSP(args, parsed_globals):
+def doCreateEdgeCVMInstances(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -3444,11 +3494,11 @@ def doModifyClusterEndpointSP(args, parsed_globals):
     client = mod.TkeClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.ModifyClusterEndpointSPRequest()
+    model = models.CreateEdgeCVMInstancesRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.ModifyClusterEndpointSP(model)
+        rsp = client.CreateEdgeCVMInstances(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -4545,7 +4595,7 @@ ACTION_MAP = {
     "DescribeTKEEdgeClusters": doDescribeTKEEdgeClusters,
     "DescribeVersions": doDescribeVersions,
     "DescribeEdgeClusterExtraArgs": doDescribeEdgeClusterExtraArgs,
-    "AddNodeToNodePool": doAddNodeToNodePool,
+    "DescribeEdgeLogSwitches": doDescribeEdgeLogSwitches,
     "DescribeTKEEdgeExternalKubeconfig": doDescribeTKEEdgeExternalKubeconfig,
     "ModifyClusterNodePool": doModifyClusterNodePool,
     "DescribeClusterAsGroups": doDescribeClusterAsGroups,
@@ -4565,11 +4615,12 @@ ACTION_MAP = {
     "UpgradeClusterInstances": doUpgradeClusterInstances,
     "DescribeClusterRouteTables": doDescribeClusterRouteTables,
     "DeleteEdgeCVMInstances": doDeleteEdgeCVMInstances,
-    "DescribeEdgeLogSwitches": doDescribeEdgeLogSwitches,
+    "AddNodeToNodePool": doAddNodeToNodePool,
     "DescribeClusters": doDescribeClusters,
     "DescribeClusterEndpointStatus": doDescribeClusterEndpointStatus,
     "DescribeRegions": doDescribeRegions,
     "AddExistedInstances": doAddExistedInstances,
+    "ModifyClusterEndpointSP": doModifyClusterEndpointSP,
     "DisableClusterDeletionProtection": doDisableClusterDeletionProtection,
     "DescribeRouteTableConflicts": doDescribeRouteTableConflicts,
     "EnableClusterDeletionProtection": doEnableClusterDeletionProtection,
@@ -4596,7 +4647,7 @@ ACTION_MAP = {
     "DeleteClusterEndpointVip": doDeleteClusterEndpointVip,
     "DescribeECMInstances": doDescribeECMInstances,
     "DescribeClusterNodePools": doDescribeClusterNodePools,
-    "ModifyClusterEndpointSP": doModifyClusterEndpointSP,
+    "CreateEdgeCVMInstances": doCreateEdgeCVMInstances,
     "DescribeClusterRoutes": doDescribeClusterRoutes,
     "ModifyNodePoolInstanceTypes": doModifyNodePoolInstanceTypes,
     "CheckInstancesUpgradeAble": doCheckInstancesUpgradeAble,
