526d05b2b811507c2b1aaa1d73e3632daac6fe9e
tencentcloudapi <tencentcloudapi@tencent.com>
2026-04-10 04:51:42 +0800

    release 3.1.72.1

--- tccli/services/hunyuan/hunyuan_client.py
+++ tccli/services/hunyuan/hunyuan_client.py
@@ -17,7 +17,7 @@ from tencentcloud.hunyuan.v20230901 import models as models_v20230901
 from jmespath import search
 import time
 
-def doSubmitHunyuanTo3DTextureEditJob(args, parsed_globals):
+def doQueryHunyuanTo3DRapidJob(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -49,11 +49,11 @@ def doSubmitHunyuanTo3DTextureEditJob(args, parsed_globals):
     client = mod.HunyuanClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.SubmitHunyuanTo3DTextureEditJobRequest()
+    model = models.QueryHunyuanTo3DRapidJobRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.SubmitHunyuanTo3DTextureEditJob(model)
+        rsp = client.QueryHunyuanTo3DRapidJob(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -182,6 +182,61 @@ def doSubmit3DSmartTopologyJob(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
+def doSubmitConvert3DFormatJob(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
+        cred = credential.CVMRoleCredential()
+    elif g_param[OptionsDefine.RoleArn.replace('-', '_')] and g_param[OptionsDefine.RoleSessionName.replace('-', '_')]:
+        cred = credential.STSAssumeRoleCredential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.RoleArn.replace('-', '_')],
+            g_param[OptionsDefine.RoleSessionName.replace('-', '_')], endpoint=g_param["sts_cred_endpoint"]
+        )
+    elif os.getenv(OptionsDefine.ENV_TKE_REGION) \
+            and os.getenv(OptionsDefine.ENV_TKE_PROVIDER_ID) \
+            and os.getenv(OptionsDefine.ENV_TKE_WEB_IDENTITY_TOKEN_FILE) \
+            and os.getenv(OptionsDefine.ENV_TKE_ROLE_ARN):
+        cred = credential.DefaultTkeOIDCRoleArnProvider().get_credentials()
+    else:
+        cred = credential.Credential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+        )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint],
+        proxy=g_param[OptionsDefine.HttpsProxy.replace('-', '_')]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="TC3-HMAC-SHA256")
+    if g_param[OptionsDefine.Language]:
+        profile.language = g_param[OptionsDefine.Language]
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.HunyuanClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.SubmitConvert3DFormatJobRequest()
+    model.from_json_string(json.dumps(args))
+    start_time = time.time()
+    while True:
+        rsp = client.SubmitConvert3DFormatJob(model)
+        result = rsp.to_json_string()
+        try:
+            json_obj = json.loads(result)
+        except TypeError as e:
+            json_obj = json.loads(result.decode('utf-8'))  # python3.3
+        if not g_param[OptionsDefine.Waiter] or search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj) == g_param['OptionsDefine.WaiterInfo']['to']:
+            break
+        cur_time = time.time()
+        if cur_time - start_time >= g_param['OptionsDefine.WaiterInfo']['timeout']:
+            raise ClientError('Request timeout, wait `%s` to `%s` timeout, last request is %s' %
+            (g_param['OptionsDefine.WaiterInfo']['expr'], g_param['OptionsDefine.WaiterInfo']['to'],
+            search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj)))
+        else:
+            print('Inquiry result is %s.' % search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj))
+        time.sleep(g_param['OptionsDefine.WaiterInfo']['interval'])
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
 def doSubmitHunyuanTo3DUVJob(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
@@ -292,7 +347,7 @@ def doSubmitHunyuanTo3DProJob(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doQueryHunyuanTo3DRapidJob(args, parsed_globals):
+def doSubmitHunyuan3DPartJob(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -324,11 +379,11 @@ def doQueryHunyuanTo3DRapidJob(args, parsed_globals):
     client = mod.HunyuanClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.QueryHunyuanTo3DRapidJobRequest()
+    model = models.SubmitHunyuan3DPartJobRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.QueryHunyuanTo3DRapidJob(model)
+        rsp = client.SubmitHunyuan3DPartJob(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -439,6 +494,61 @@ def doDescribe3DSmartTopologyJob(args, parsed_globals):
     start_time = time.time()
     while True:
         rsp = client.Describe3DSmartTopologyJob(model)
+        result = rsp.to_json_string()
+        try:
+            json_obj = json.loads(result)
+        except TypeError as e:
+            json_obj = json.loads(result.decode('utf-8'))  # python3.3
+        if not g_param[OptionsDefine.Waiter] or search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj) == g_param['OptionsDefine.WaiterInfo']['to']:
+            break
+        cur_time = time.time()
+        if cur_time - start_time >= g_param['OptionsDefine.WaiterInfo']['timeout']:
+            raise ClientError('Request timeout, wait `%s` to `%s` timeout, last request is %s' %
+            (g_param['OptionsDefine.WaiterInfo']['expr'], g_param['OptionsDefine.WaiterInfo']['to'],
+            search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj)))
+        else:
+            print('Inquiry result is %s.' % search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj))
+        time.sleep(g_param['OptionsDefine.WaiterInfo']['interval'])
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doDescribeConvert3DFormatJob(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
+        cred = credential.CVMRoleCredential()
+    elif g_param[OptionsDefine.RoleArn.replace('-', '_')] and g_param[OptionsDefine.RoleSessionName.replace('-', '_')]:
+        cred = credential.STSAssumeRoleCredential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.RoleArn.replace('-', '_')],
+            g_param[OptionsDefine.RoleSessionName.replace('-', '_')], endpoint=g_param["sts_cred_endpoint"]
+        )
+    elif os.getenv(OptionsDefine.ENV_TKE_REGION) \
+            and os.getenv(OptionsDefine.ENV_TKE_PROVIDER_ID) \
+            and os.getenv(OptionsDefine.ENV_TKE_WEB_IDENTITY_TOKEN_FILE) \
+            and os.getenv(OptionsDefine.ENV_TKE_ROLE_ARN):
+        cred = credential.DefaultTkeOIDCRoleArnProvider().get_credentials()
+    else:
+        cred = credential.Credential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+        )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint],
+        proxy=g_param[OptionsDefine.HttpsProxy.replace('-', '_')]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="TC3-HMAC-SHA256")
+    if g_param[OptionsDefine.Language]:
+        profile.language = g_param[OptionsDefine.Language]
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.HunyuanClient(cred, g_param[OptionsDefine.Region], profile)
+    client._sdkVersion += ("_CLI_" + __version__)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.DescribeConvert3DFormatJobRequest()
+    model.from_json_string(json.dumps(args))
+    start_time = time.time()
+    while True:
+        rsp = client.DescribeConvert3DFormatJob(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -732,7 +842,7 @@ def doSubmitHunyuanTo3DRapidJob(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doSubmitHunyuan3DPartJob(args, parsed_globals):
+def doSubmitHunyuanTo3DTextureEditJob(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -764,11 +874,11 @@ def doSubmitHunyuan3DPartJob(args, parsed_globals):
     client = mod.HunyuanClient(cred, g_param[OptionsDefine.Region], profile)
     client._sdkVersion += ("_CLI_" + __version__)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.SubmitHunyuan3DPartJobRequest()
+    model = models.SubmitHunyuanTo3DTextureEditJobRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.SubmitHunyuan3DPartJob(model)
+        rsp = client.SubmitHunyuanTo3DTextureEditJob(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -798,20 +908,22 @@ MODELS_MAP = {
 }
 
 ACTION_MAP = {
-    "SubmitHunyuanTo3DTextureEditJob": doSubmitHunyuanTo3DTextureEditJob,
+    "QueryHunyuanTo3DRapidJob": doQueryHunyuanTo3DRapidJob,
     "Convert3DFormat": doConvert3DFormat,
     "Submit3DSmartTopologyJob": doSubmit3DSmartTopologyJob,
+    "SubmitConvert3DFormatJob": doSubmitConvert3DFormatJob,
     "SubmitHunyuanTo3DUVJob": doSubmitHunyuanTo3DUVJob,
     "SubmitHunyuanTo3DProJob": doSubmitHunyuanTo3DProJob,
-    "QueryHunyuanTo3DRapidJob": doQueryHunyuanTo3DRapidJob,
+    "SubmitHunyuan3DPartJob": doSubmitHunyuan3DPartJob,
     "QueryHunyuan3DPartJob": doQueryHunyuan3DPartJob,
     "Describe3DSmartTopologyJob": doDescribe3DSmartTopologyJob,
+    "DescribeConvert3DFormatJob": doDescribeConvert3DFormatJob,
     "QueryHunyuanTo3DTextureEditJob": doQueryHunyuanTo3DTextureEditJob,
     "DescribeHunyuanTo3DUVJob": doDescribeHunyuanTo3DUVJob,
     "ChatTranslations": doChatTranslations,
     "QueryHunyuanTo3DProJob": doQueryHunyuanTo3DProJob,
     "SubmitHunyuanTo3DRapidJob": doSubmitHunyuanTo3DRapidJob,
-    "SubmitHunyuan3DPartJob": doSubmitHunyuan3DPartJob,
+    "SubmitHunyuanTo3DTextureEditJob": doSubmitHunyuanTo3DTextureEditJob,
 
 }
 
