58ae4f579dde59252f092afb54ceb7469ead7b31
tencentcloudapi <tencentcloudapi@tencent.com>
2026-09-08 06:38:06 +0800

    release 3.1.169.1

--- tccli/services/antiddos/antiddos_client.py
+++ tccli/services/antiddos/antiddos_client.py
@@ -703,7 +703,7 @@ def doDescribeCCPrecisionPlyList(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doDescribeListSchedulingDomain(args, parsed_globals):
+def doDescribeOverviewDDoSEventList(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -737,11 +737,11 @@ def doDescribeListSchedulingDomain(args, parsed_globals):
     mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
     client = mod.AntiddosClient(cred, g_param[OptionsDefine.Region], profile)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.DescribeListSchedulingDomainRequest()
+    model = models.DescribeOverviewDDoSEventListRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.DescribeListSchedulingDomain(model)
+        rsp = client.DescribeOverviewDDoSEventList(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -1330,7 +1330,7 @@ def doCreateBoundIP(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doDescribeOverviewDDoSEventList(args, parsed_globals):
+def doCreateBgpInstance(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -1364,11 +1364,11 @@ def doDescribeOverviewDDoSEventList(args, parsed_globals):
     mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
     client = mod.AntiddosClient(cred, g_param[OptionsDefine.Region], profile)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.DescribeOverviewDDoSEventListRequest()
+    model = models.CreateBgpInstanceRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.DescribeOverviewDDoSEventList(model)
+        rsp = client.CreateBgpInstance(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -1387,7 +1387,7 @@ def doDescribeOverviewDDoSEventList(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doDescribeCCTrend(args, parsed_globals):
+def doDescribeListSchedulingDomain(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -1421,11 +1421,68 @@ def doDescribeCCTrend(args, parsed_globals):
     mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
     client = mod.AntiddosClient(cred, g_param[OptionsDefine.Region], profile)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.DescribeCCTrendRequest()
+    model = models.DescribeListSchedulingDomainRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.DescribeCCTrend(model)
+        rsp = client.DescribeListSchedulingDomain(model)
+        result = rsp.to_json_string()
+        try:
+            json_obj = json.loads(result)
+        except TypeError as e:
+            json_obj = json.loads(result.decode('utf-8'))  # python3.3
+        if not g_param[OptionsDefine.Waiter] or search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj) == g_param['OptionsDefine.WaiterInfo']['to']:
+            break
+        cur_time = time.time()
+        if cur_time - start_time >= g_param['OptionsDefine.WaiterInfo']['timeout']:
+            raise ClientError('Request timeout, wait `%s` to `%s` timeout, last request is %s' %
+            (g_param['OptionsDefine.WaiterInfo']['expr'], g_param['OptionsDefine.WaiterInfo']['to'],
+            search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj)))
+        else:
+            print('Inquiry result is %s.' % search(g_param['OptionsDefine.WaiterInfo']['expr'], json_obj))
+        time.sleep(g_param['OptionsDefine.WaiterInfo']['interval'])
+    FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
+
+
+def doModifyDDoSSpeedLimitConfig(args, parsed_globals):
+    g_param = parse_global_arg(parsed_globals)
+
+    if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
+        cred = credential.CVMRoleCredential()
+    elif g_param[OptionsDefine.RoleArn.replace('-', '_')] and g_param[OptionsDefine.RoleSessionName.replace('-', '_')]:
+        cred = credential.STSAssumeRoleCredential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.RoleArn.replace('-', '_')],
+            g_param[OptionsDefine.RoleSessionName.replace('-', '_')], endpoint=g_param["sts_cred_endpoint"]
+        )
+    elif os.getenv(OptionsDefine.ENV_TKE_REGION) \
+            and os.getenv(OptionsDefine.ENV_TKE_PROVIDER_ID) \
+            and os.getenv(OptionsDefine.ENV_TKE_WEB_IDENTITY_TOKEN_FILE) \
+            and os.getenv(OptionsDefine.ENV_TKE_ROLE_ARN):
+        cred = credential.DefaultTkeOIDCRoleArnProvider().get_credentials()
+    else:
+        cred = credential.Credential(
+            g_param[OptionsDefine.SecretId], g_param[OptionsDefine.SecretKey], g_param[OptionsDefine.Token]
+        )
+    http_profile = HttpProfile(
+        reqTimeout=60 if g_param[OptionsDefine.Timeout] is None else int(g_param[OptionsDefine.Timeout]),
+        reqMethod="POST",
+        endpoint=g_param[OptionsDefine.Endpoint],
+        proxy=g_param[OptionsDefine.HttpsProxy.replace('-', '_')]
+    )
+    profile = ClientProfile(httpProfile=http_profile, signMethod="TC3-HMAC-SHA256")
+    profile.request_client = "_CLI_" + __version__
+    if g_param[OptionsDefine.RequestClient.replace('-', '_')]:
+        profile.request_client += "; " + g_param[OptionsDefine.RequestClient.replace('-', '_')]
+    if g_param[OptionsDefine.Language]:
+        profile.language = g_param[OptionsDefine.Language]
+    mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
+    client = mod.AntiddosClient(cred, g_param[OptionsDefine.Region], profile)
+    models = MODELS_MAP[g_param[OptionsDefine.Version]]
+    model = models.ModifyDDoSSpeedLimitConfigRequest()
+    model.from_json_string(json.dumps(args))
+    start_time = time.time()
+    while True:
+        rsp = client.ModifyDDoSSpeedLimitConfig(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -1900,7 +1957,7 @@ def doCreateDDoSSpeedLimitConfig(args, parsed_globals):
     FormatOutput.output("action", json_obj, g_param[OptionsDefine.Output], g_param[OptionsDefine.Filter])
 
 
-def doModifyDDoSSpeedLimitConfig(args, parsed_globals):
+def doDescribeCCTrend(args, parsed_globals):
     g_param = parse_global_arg(parsed_globals)
 
     if g_param[OptionsDefine.UseCVMRole.replace('-', '_')]:
@@ -1934,11 +1991,11 @@ def doModifyDDoSSpeedLimitConfig(args, parsed_globals):
     mod = CLIENT_MAP[g_param[OptionsDefine.Version]]
     client = mod.AntiddosClient(cred, g_param[OptionsDefine.Region], profile)
     models = MODELS_MAP[g_param[OptionsDefine.Version]]
-    model = models.ModifyDDoSSpeedLimitConfigRequest()
+    model = models.DescribeCCTrendRequest()
     model.from_json_string(json.dumps(args))
     start_time = time.time()
     while True:
-        rsp = client.ModifyDDoSSpeedLimitConfig(model)
+        rsp = client.DescribeCCTrend(model)
         result = rsp.to_json_string()
         try:
             json_obj = json.loads(result)
@@ -4148,7 +4205,7 @@ ACTION_MAP = {
     "DeleteDDoSGeoIPBlockConfig": doDeleteDDoSGeoIPBlockConfig,
     "ModifyPacketFilterConfig": doModifyPacketFilterConfig,
     "DescribeCCPrecisionPlyList": doDescribeCCPrecisionPlyList,
-    "DescribeListSchedulingDomain": doDescribeListSchedulingDomain,
+    "DescribeOverviewDDoSEventList": doDescribeOverviewDDoSEventList,
     "CreatePacketFilterConfig": doCreatePacketFilterConfig,
     "DescribeListIPAlarmConfig": doDescribeListIPAlarmConfig,
     "DescribeListBGPIPInstances": doDescribeListBGPIPInstances,
@@ -4159,8 +4216,9 @@ ACTION_MAP = {
     "CreateDefaultAlarmThreshold": doCreateDefaultAlarmThreshold,
     "DescribeCCLevelPolicy": doDescribeCCLevelPolicy,
     "CreateBoundIP": doCreateBoundIP,
-    "DescribeOverviewDDoSEventList": doDescribeOverviewDDoSEventList,
-    "DescribeCCTrend": doDescribeCCTrend,
+    "CreateBgpInstance": doCreateBgpInstance,
+    "DescribeListSchedulingDomain": doDescribeListSchedulingDomain,
+    "ModifyDDoSSpeedLimitConfig": doModifyDDoSSpeedLimitConfig,
     "DescribeNewL7RulesErrHealth": doDescribeNewL7RulesErrHealth,
     "DescribeCcBlackWhiteIpList": doDescribeCcBlackWhiteIpList,
     "CreateWaterPrintConfig": doCreateWaterPrintConfig,
@@ -4169,7 +4227,7 @@ ACTION_MAP = {
     "DescribeBgpBizTrend": doDescribeBgpBizTrend,
     "CreateCCPrecisionPolicy": doCreateCCPrecisionPolicy,
     "CreateDDoSSpeedLimitConfig": doCreateDDoSSpeedLimitConfig,
-    "ModifyDDoSSpeedLimitConfig": doModifyDDoSSpeedLimitConfig,
+    "DescribeCCTrend": doDescribeCCTrend,
     "CreateIPAlarmThresholdConfig": doCreateIPAlarmThresholdConfig,
     "DeleteCCLevelPolicy": doDeleteCCLevelPolicy,
     "DescribeBizTrend": doDescribeBizTrend,
